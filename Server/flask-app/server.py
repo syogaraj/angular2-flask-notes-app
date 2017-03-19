fc25216cd49f5c3714d7b8ea50230f24fa6b4a17
yoga30696 <yoga30696@gmail.com>
2017-03-19 20:29:34 +0530

    added comments

--- Server/flask-app/server.py
+++ Server/flask-app/server.py
@@ -3,24 +3,27 @@ from flask_cors import CORS
 import os, subprocess
 
 app = Flask(__name__)
-CORS(app)
+CORS(app) #Cross origin to accept request from angular client
 
 @app.route('/notes/<username>', methods=['GET'])
 def getnotes(username):
+    
     os.chdir("../client-notes")
+    #open file and read the data
     notefile = open(username, 'a+')
     notesdata = notefile.read()
     notefile.close()
-    print "Data:", notesdata
     os.chdir("../flask-app")
+    # send the read data as response of type text/plain
     return Response(notesdata, mimetype="text/plain")
 
 @app.route('/post/<username>', methods=['POST'])
 def writenotes(username):
+    
     os.chdir("../client-notes")
+    #open/ create file and write data from client POST method
     notefile = open(username, 'w+')
     data = request.get_data()
-    print data
     notefile.write(str(data))
     return Response(data, mimetype="text/plain")
 
