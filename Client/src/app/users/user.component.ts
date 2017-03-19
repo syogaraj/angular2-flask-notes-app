3d88c08c35e501152be4e5852181fa4b71ad2279
yoga30696 <yoga30696@gmail.com>
2017-03-19 19:06:58 +0530

    added get method

--- Client/src/app/users/user.component.ts
+++ Client/src/app/users/user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { Http, Response } from '@angular/http';
 
 @Component({
     selector: 'user',
@@ -16,14 +17,20 @@ import { NavbarComponent } from '../navbar/navbar.component';
 
 export class UserComponent{
     username: any;
-    notes: string;
-    constructor(private route: ActivatedRoute) { 
+    notes: any;
+    appurl = "http://localhost:5000/notes/"+this.username;
+
+    constructor(private http: Http, private route: ActivatedRoute) { 
         this.username = this.route.snapshot.params['username']
         console.log(this.username);
     }
 
     ngOnInit(){
-        
+        this.http.get(this.appurl).subscribe(
+            data=> {
+                this.notes=data;
+            }
+        )
     }
     Save(): void{
         console.log('Clicked -- save '+this.notes);
