5a3a0de03877d27cfcbdcb14af6fe4248aaa6203
yoga30696 <yoga30696@gmail.com>
2017-03-19 21:34:10 +0530

    added url input to app.component.html and action in ts files

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent {
   
   newnote: any;
   url: any;
+  showUrl: boolean = true;
   createnote(){
     this.url = "http://localhost:4200/notes/"+this.newnote;
     console.log(this.url);
